b53373bca2fd15b450d23d16838f34f84fa37deb
Roshni <roshni.mali@bridgelabz.com>
2024-09-12 12:09:02 +0530

    Added: Restricting the pincode from taking alphabets or special character at the end

--- com/bridgelabz/regexpractice/PincodeRegex.java
+++ com/bridgelabz/regexpractice/PincodeRegex.java
@@ -9,11 +9,13 @@ public class PincodeRegex {
     * PIN Code UC1: The Postal Index Number (PIN) or PIN Code is a 6 digit code
         of Post Office numbering used by India Post.
         Create a regex pattern to validate Pin Code 400088
-
-    * /*
+        *
     * PIN Code UC2: Restrict the PIN code from taking alphabets or special characters at the
     beginning.
     Check for A400088 – this should fail
+    *
+    * PIN Code UC3: Restrict the PIN code from taking alphabets or special characters at the End.
+    Check for 400088B – this should fail
     * */
     private static final String PINCODE_PATTERN = "^[1-9][0-9]{5}$";
     public static boolean validatePinCode(String pincode){
@@ -23,7 +25,7 @@ public class PincodeRegex {
     }
 
     public static void main(String[] args) {
-        String pincodeToValidate = "A400088"; // this input should fail
+        String pincodeToValidate = "400088B"; // this input should fail
 
         if(validatePinCode(pincodeToValidate)){
             System.out.println(pincodeToValidate+ " is valid pincode.");
